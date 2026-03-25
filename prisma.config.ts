c4f5149f50022582066c36265a2ef48d47bc4dd2
zoynulabedin <trustcodernet@gmail.com>
2026-03-25 22:50:50 +0600

    update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,4 @@
-import { resolve, dirname } from "path";
-import { fileURLToPath } from "url";
-import { config } from "dotenv";
 import { defineConfig } from "prisma/config";
-
-config({ path: resolve(dirname(fileURLToPath(import.meta.url)), ".env") });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
